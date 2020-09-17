551875d4039288869386e7d2e5a49c54fb9649a2
Slambert-S <71460520+Slambert-S@users.noreply.github.com>
2020-09-17 14:38:10 -0400

    Create exit.cpp

---
+++ exit.cpp
@@ -0,0 +1,7 @@
+#include<stdlib.h>
+int main() {
+	// The exit() function can be used to terminate the program. 
+	// The exit() function is part of the stdlib.h library. 
+	// Calling the exit() in the main function is functionally equivalent to return(). 
+	exit (0); 
+}
