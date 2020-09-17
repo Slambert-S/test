b5cee6020a01a4f03b8ee23d9451274ec0fc61e0
Slambert-S <71460520+Slambert-S@users.noreply.github.com>
2020-09-17 14:40:03 -0400

    Update exit.cpp

--- exit.cpp
+++ exit.cpp
@@ -2,6 +2,9 @@
 int main() {
 	// The exit() function can be used to terminate the program. 
 	// The exit() function is part of the stdlib.h library. 
+
+
+
 	// Calling the exit() in the main function is functionally equivalent to return(). 
 	exit (0); 
 }
